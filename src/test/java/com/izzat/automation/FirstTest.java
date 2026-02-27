1ef2329f643c4db047004514f858126c5747a7e7
Agustuscaeza01 <mhdizzat2001@gmail.com>
2026-02-27 10:05:32 +0800

    Initial commit

---
+++ src/test/java/com/izzat/automation/FirstTest.java
@@ -0,0 +1,21 @@
+package com.izzat.automation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+public class FirstTest {
+
+    @Test
+    public void openSauceDemo() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.saucedemo.com/");
+        System.out.println("Title is: " + driver.getTitle());
+        driver.quit();
+    }
+}
